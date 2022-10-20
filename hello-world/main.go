c993bd71631d84ed399dcb43246f590460f15eb1
ilhammfadilah <ilhamfadilah92@gmail.com>
2022-10-20 13:24:32 +0700

    belajar hello world

---
+++ hello-world/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+// entry point main function
+func main() {
+	// print to display
+	fmt.Println("Hello, World!");
+}
+
+/*
+	belajar golang
+	menggunakan komentar multiple line
+*/
