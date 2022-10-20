489fc8919f47cd2239e694625c16b8ca847f5ded
ilhammfadilah <ilhamfadilah92@gmail.com>
2022-10-20 13:37:43 +0700

    operator logika

--- operator/main.go
+++ operator/main.go
@@ -5,6 +5,17 @@ import "fmt"
 func main() {
 	var value = (((2 + 6) % 3)* 4 - 2)/ 3
 	var isEqual = (value == 2) // perbandingan
-
 	fmt.Printf("nilai %d (%t) \n", value, isEqual)
+
+	var left = false
+	var right = true
+
+	var leftAndRight = left && right
+	fmt.Printf("left and right \t(%t) \n", leftAndRight)
+
+	var leftOrRight = left || right
+	fmt.Printf("left or right \t(%t) \n", leftOrRight)
+
+	var leftReverse = !left
+	fmt.Printf("!left \t\t(%t) \n", leftReverse)
 }
