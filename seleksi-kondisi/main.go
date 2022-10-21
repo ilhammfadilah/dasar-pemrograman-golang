719bc0aa69f93fca3db9c510b60c079a69092136
ilhammfadilah <ilhamfadilah92@gmail.com>
2022-10-21 20:17:12 +0700

    seleksi kondisi bersarang

--- seleksi-kondisi/main.go
+++ seleksi-kondisi/main.go
@@ -91,4 +91,24 @@ func main() {
 		}
 	}
 
+	// seleksi dengan kondisi bersarang
+	if point > 7 {
+		switch point {
+		case 10:
+			fmt.Println("perfect!")
+		default:
+			fmt.Println("nice!")
+		}
+	} else {
+		if point == 5 {
+			fmt.Println("not bad")
+		} else if point == 3 {
+			fmt.Println("keep trying")
+		} else {
+			fmt.Println("you can do id")
+			if point == 0 {
+				fmt.Println("try harder!")
+			}
+		}
+	}
 }
