ee56700f3af662762cbe20245fd9b53b64080a2f
ilhammfadilah <ilhamfadilah92@gmail.com>
2022-10-21 19:51:52 +0700

    if - else dengan temporary variabel

--- seleksi-kondisi/main.go
+++ seleksi-kondisi/main.go
@@ -14,6 +14,17 @@ func main() {
 	} else if point == 4 {
 		fmt.Println("hampir lulus")
 	} else {
-		fmt.Printf("maaf tidak lulus, nilai anda %d", point)
+		fmt.Printf("maaf tidak lulus, nilai anda %d \n", point)
 	}
+
+	// selekse if - else variabel temporary
+	var skor = 8840.0
+	if percent := skor / 100; percent >= 100 {
+		fmt.Printf("%.1f%s perfect!\n", percent, "%")
+	} else if percent >= 70 {
+		fmt.Printf("%.1f%s good!\n", percent, "%")
+	} else {
+		fmt.Printf("%.1f%s not bad! \n", percent, "%")
+	}
+
 }
