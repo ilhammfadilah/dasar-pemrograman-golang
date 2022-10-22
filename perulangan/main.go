097ec317bd7440da740e9463352c637588d37b1d
ilhammfadilah <ilhamfadilah92@gmail.com>
2022-10-22 13:26:02 +0700

    perulangan bersarang

--- perulangan/main.go
+++ perulangan/main.go
@@ -37,4 +37,12 @@ func main() {
 
 		fmt.Println("Angka", i)
 	}
+
+	// perulangan bersarang
+	for i := 0; i < 5; i++ {
+		for j := i; j < 5; j++ {
+			fmt.Print(j, " ")
+		}
+		fmt.Println()
+	}
 }
