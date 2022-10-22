386f4042995544ac5c3d0cb598941e283ffba52f
ilhammfadilah <ilhamfadilah92@gmail.com>
2022-10-22 13:36:49 +0700

    label dalam perulangan

--- perulangan/main.go
+++ perulangan/main.go
@@ -45,4 +45,30 @@ func main() {
 		}
 		fmt.Println()
 	}
+
+	// label dalam perulangan
+	outerloop:
+	for i := 0; i < 5; i++ {
+		for j := 0; j < 5; j++ {
+			if i == 3 {
+				break outerloop
+			}
+			fmt.Print("matriks [", i, "][", j, "]", "\n")
+		}
+	}
+
+	// 1. i = 0
+	// 2. j = 0
+	// 3. print matriks[0][0]
+	// 4. i = 0
+	// 5. j = 1
+	// 6. print matriks[0][1]
+	// 7. i = 0
+	// 8. j = 2
+	// 6. print matriks[0][2]
+	// ...
+	// 7. i = 0
+	// 7. j = 4
+	// 8. print matriks[0][4]
+	// ...
 }
