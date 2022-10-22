1f0a1cf15cfcf4aee083d90725e2d588c77cf12d
ilhammfadilah <ilhamfadilah92@gmail.com>
2022-10-22 11:10:32 +0700

    for dengan hanya kondisi

--- perulangan/main.go
+++ perulangan/main.go
@@ -7,4 +7,11 @@ func main() {
 	for i := 0; i < 5; i++ {
 		fmt.Println("Angka", i)
 	}
+
+	// perulangan keyword for dengan argumen hanya kondisi
+	var n = 0
+	for n < 5 {
+		fmt.Println("Angka", n)
+		n++
+	}
 }
