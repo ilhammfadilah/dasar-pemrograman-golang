9711b8ca20bd94e2342ff6857f54a9ca06718bee
ilhammfadilah <ilhamfadilah92@gmail.com>
2022-10-22 13:21:39 +0700

    penggunaan break dan continue

--- perulangan/main.go
+++ perulangan/main.go
@@ -24,4 +24,17 @@ func main() {
 			break
 		}
 	}
+
+	// penggunaan break dan continue
+	for i := 1; i <= 10; i++ {
+		if i % 2 == 1 {
+			continue
+		}
+
+		if i > 8 {
+			break
+		}
+
+		fmt.Println("Angka", i)
+	}
 }
