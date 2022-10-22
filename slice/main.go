d78c8d4ebc6f39278e36aeee8c9be60728a9a924
ilhammfadilah <ilhamfadilah92@gmail.com>
2022-10-22 15:31:13 +0700

    slice

---
+++ slice/main.go
@@ -0,0 +1,37 @@
+/*
+Slice adalah reference elemen array. Slice bisa dibuat, atau bisa juga dihasilkan dari manipulasi sebuah array ataupun slice lainnya.
+Karena merupakan data reference, menjadikan perubahan data di tiap elemen slice akan berdampak pada slice lain yang memiliki
+alamat memori yang sama.
+*/
+
+package main
+
+import "fmt"
+
+func main() {
+	var fruits = []string{"apple", "grape", "banana", "melon"}
+
+	var aFruits = fruits[0:3]
+	var bFruits = fruits[1:4]
+
+	var aaFruits = aFruits[1:2]
+	var baFruits = bFruits[0:1]
+
+	fmt.Println("fruits: ",fruits)
+	fmt.Println("aFruits: ",aFruits)
+	fmt.Println("bFruits: ",bFruits)
+
+	fmt.Println("aaFruits: ",aaFruits)
+	fmt.Println("baFruits: ",baFruits)
+
+	baFruits[0] = "pinaple"
+
+	fmt.Println("fruits: ",fruits)
+	fmt.Println("aFruits: ",aFruits)
+	fmt.Println("bFruits: ",bFruits)
+
+	fmt.Println("aaFruits: ",aaFruits)
+	fmt.Println("baFruits: ",baFruits)
+
+
+}
