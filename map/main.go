f989b74b71add08cb34a8c771e7dce4f2825399c
ilhammfadilah <ilhamfadilah92@gmail.com>
2022-10-24 09:21:04 +0700

    Menghapus Item Map

--- map/main.go
+++ map/main.go
@@ -17,4 +17,20 @@ func main() {
 	// inisialisasi nilai map
 	var chicken1 = map[string]int{"januari": 50, "februari": 40}
 	fmt.Println(chicken1["februari"])
+
+	// Iterasi Item Map Menggunakan for - range
+	var egg = map[string]int{
+		"januari": 10,
+		"februari": 20,
+		"maret": 30,
+		"april": 40,
+	}
+
+	for key, val := range egg {
+		fmt.Println(key, "\t :", val)
+	}
+
+	// Menghapus Item Map
+	delete(egg, "april")
+	fmt.Println(egg)
 }
