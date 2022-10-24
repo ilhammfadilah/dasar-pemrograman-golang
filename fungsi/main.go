143d65b420bc9cd973ad7962f5cec184ea2ca832
ilhammfadilah <ilhamfadilah92@gmail.com>
2022-10-24 10:17:49 +0700

    fungsi dengan return value

--- fungsi/main.go
+++ fungsi/main.go
@@ -1,16 +1,26 @@
 package main
 
-import {
+import (
 	"fmt"
 	"strings"
 	"math/rand"
 	"time"
-}
+)
 
 func main() {
 	// penerapan fungsi
-	var names = []string{"John", "Wick"}
+	// var names = []string{"John", "Wick"}
 	// printMessage("halo", names)
+
+	rand.Seed(time.Now().Unix())
+	var randomValue int
+
+	randomValue = randomWithRange(2, 10)
+	fmt.Println("random number :", randomValue)
+	randomValue = randomWithRange(2, 10)
+	fmt.Println("random number :", randomValue)
+	randomValue = randomWithRange(2, 10)
+	fmt.Println("random number :", randomValue)
 }
 
 func printMessage(message string, arr []string) {
@@ -19,3 +29,7 @@ func printMessage(message string, arr []string) {
 }
 
 // fungsi dengan return value
+func randomWithRange(min, max int) int {
+	var value = rand.Int() % (max - min + 1) + min
+	return value
+}
