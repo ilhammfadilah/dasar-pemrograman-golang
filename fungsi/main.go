05e6048c5cfe442c4ce0d65a7c6fe51ebf16d308
ilhammfadilah <ilhamfadilah92@gmail.com>
2022-10-24 10:45:57 +0700

    fungsi multiple retur

--- fungsi/main.go
+++ fungsi/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"strings"
+	"math"
 	"math/rand"
 	"time"
 )
@@ -25,6 +26,12 @@ func main() {
 	divideNumber(10, 2)
 	divideNumber(4, 0)
 	divideNumber(8, -4)
+
+	var diameter float64 = 15
+	var area, circumference = calculate(diameter)
+
+	fmt.Printf("luas lingkaran \t\t: %.2f \n", area)
+	fmt.Printf("keliling lingkaran \t: %.2f \n", circumference)
 }
 
 func printMessage(message string, arr []string) {
@@ -46,4 +53,15 @@ func divideNumber(m, n int) {
 	}
 	var res = m / n
 	fmt.Printf("%d / %d = %d \n", m, n, res)
+}
+
+// fungsi multiple return
+func calculate(d float64) (float64, float64) {
+	// hitung luas
+	var area = math.Pi * math.Pow(d / 2, 2)
+	// hitung keliling
+	var circumference = math.Pi * d
+
+	// kembalikan 2 nilai
+	return area, circumference
 }
