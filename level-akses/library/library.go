d121dcbbb48481bd4eea8162a401419757dd94ea
ilhammfadilah <ilhamfadilah92@gmail.com>
2022-10-25 17:11:00 +0700

    properti public dan private

--- level-akses/library/library.go
+++ level-akses/library/library.go
@@ -1,10 +1,15 @@
 package library
-import "fmt"
+// import "fmt"
 
-func SayHello(name string) {
-	fmt.Println("hello")
-	introduce(name)
-}
-func introduce(name string) {
-	fmt.Println("nama saya", name)
+// func SayHello(name string) {
+// 	fmt.Println("hello")
+// 	introduce(name)
+// }
+// func introduce(name string) {
+// 	fmt.Println("nama saya", name)
+// }
+
+type Student struct {
+	Name string
+	Grade int
 }
--- level-akses/main.go
+++ level-akses/main.go
@@ -1,7 +1,12 @@
 package main
 import "level-akses/library"
+import "fmt"
 
 func main() {
-	library.SayHello("ilham")
+	// library.SayHello("ilham")
 	// library.introduce("ilham")
+
+	var student1 = library.Student{"ilham", 9}
+	fmt.Println("student 1 name: ", student1.Name)
+	fmt.Println("student 1 grade: ", student1.Grade)
 }
