94be2beefdd35d0893989f5fa0b1ebcc5bc961b2
ilhammfadilah <ilhamfadilah92@gmail.com>
2022-10-25 17:14:07 +0700

    import dengan prefix tanda titik

--- level-akses/main.go
+++ level-akses/main.go
@@ -1,12 +1,18 @@
 package main
-import "level-akses/library"
-import "fmt"
+import (
+	. "level-akses/library"
+	"fmt"
+)
 
 func main() {
 	// library.SayHello("ilham")
 	// library.introduce("ilham")
 
-	var student1 = library.Student{"ilham", 9}
-	fmt.Println("student 1 name: ", student1.Name)
-	fmt.Println("student 1 grade: ", student1.Grade)
+	// var student1 = library.Student{"ilham", 9}
+	// fmt.Println("student 1 name: ", student1.Name)
+	// fmt.Println("student 1 grade: ", student1.Grade)
+
+	var student2 = Student{"fadilah", 8}
+	fmt.Println("student 2 name: ", student2.Name)
+	fmt.Println("student 2 grade: ", student2.Grade)
 }
