3a9a1b2763ae7087c10bfd1bb3acc5bceed8f0e7
ilhammfadilah <ilhamfadilah92@gmail.com>
2022-10-25 11:24:57 +0700

    pengisian sub struct

--- struct/main.go
+++ struct/main.go
@@ -11,7 +11,8 @@ func main() {
 	s1.name = "ilham"
 	s1.grade = 9
 
-	var s2 = student{"muhamad", 10}
+	var p2 = person{age: 9}
+	var s2 = student{"muhamad", 10, p2}
 
 	fmt.Println("name :", s1.name)
 	fmt.Println("grade :", s1.grade)
@@ -19,6 +20,7 @@ func main() {
 	fmt.Println("name :", s2.name)
 	fmt.Println("grade :", s2.grade)
 
+	// variabel objek pointer
 	var s3 = student{name: "fadilah", grade: 9}
 	var s4 *student = &s3
 	fmt.Println("student 3 name: ", s3.name)
@@ -27,9 +29,25 @@ func main() {
 	s3.name = "agung"
 	fmt.Println("student 3 name: ", s3.name)
 	fmt.Println("student 4 name: ", s4.name)
+
+	// embedded struct
+	var s5 = student{}
+	s5.name = "mona <3"
+	s5.age = 22
+	s5.grade = 9
+
+	fmt.Println("student 5 name: ",s5.name)
+	fmt.Println("student 5 age: ",s5.person.age)
+	fmt.Println("student 5 grade: ",s5.grade)
+
 }
 
 type student struct {
 	name string
 	grade int
+	person
+}
+
+type person struct {
+	age int
 }
