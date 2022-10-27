afb10754c47d80ed24747318e3c87a9974705f38
ilhammfadilah <ilhamfadilah92@gmail.com>
2022-10-28 04:49:56 +0700

    Konversi Angka ke time.Duration

--- time-duration/main.go
+++ time-duration/main.go
@@ -6,13 +6,30 @@ import (
 )
 
 func main() {
-	start := time.Now()
+	// start := time.Now()
 
-	time.Sleep(5 * time.Second)
+	// time.Sleep(5 * time.Second)
 
-	duration := time.Since(start)
+	// duration := time.Since(start)
 
-	fmt.Println("Time elapsed in seconds ", duration.Seconds())
-	fmt.Println("Time elapsed in minutes ", duration.Minutes())
-	fmt.Println("Time elapsed in hours ", duration.Hours())
+	// fmt.Println("Time elapsed in seconds ", duration.Seconds())
+	// fmt.Println("Time elapsed in minutes ", duration.Minutes())
+	// fmt.Println("Time elapsed in hours ", duration.Hours())
+
+	// Kalkulasi Durasi Antara 2 Objek Waktu
+
+	// time1 := time.Now()
+	// time.Sleep(5 * time.Second)
+	// time2 := time.Now()
+
+	// duration := time2.Sub(time1)
+	// fmt.Println("Time elapsed in seconds ", duration.Seconds())
+	// fmt.Println("Time elapsed in minutes ", duration.Minutes())
+	// fmt.Println("Time elapsed in hours ", duration.Hours())
+
+	// Konversi Angka ke time.Duration
+
+	var n time.Duration = 60
+	duration := n * time.Second
+	fmt.Println(duration)
 }
