0d0d769b8d4e879929936cf9b294da95658e5c7c
ilhammfadilah <ilhamfadilah92@gmail.com>
2022-10-31 18:33:06 +0700

    Encode & Decode Data URL

--- encode-decode-base64/main.go
+++ encode-decode-base64/main.go
@@ -6,12 +6,32 @@ import (
 )
 
 func main() {
-	var data = "john wick"
+	// var data = "john wick"
 
-	var encodingString = base64.StdEncoding.EncodeToString([]byte(data))
-	fmt.Println("encode : ", encodingString)
+	// var encodingString = base64.StdEncoding.EncodeToString([]byte(data))
+	// fmt.Println("encode : ", encodingString)
 
-	var decodeByte, _ = base64.StdEncoding.DecodeString(encodingString)
-	var decodeString = string(decodeByte)
-	fmt.Println("decode : ", decodeString)
+	// var decodeByte, _ = base64.StdEncoding.DecodeString(encodingString)
+	// var decodeString = string(decodeByte)
+	// fmt.Println("decode : ", decodeString)
+
+	// var encoded = make([]byte, base64.StdEncoding.EncodedLen(len(data)))
+	// base64.StdEncoding.Encode(encoded, []byte(data))
+	// var encodedString = string(encoded)
+	// fmt.Println(encodedString)
+
+	// var decoded = make([]byte, base64.StdEncoding.DecodedLen(len(encoded)))
+	// var _, err = base64.StdEncoding.Decode(decoded, encoded)
+	// if err != nil {
+	// 	fmt.Println(err.Error())
+	// }
+	// var decodedString = string(decoded)
+	// fmt.Println(decodedString)
+
+	var url = "https://kalipare.com/"
+	var encodedString = base64.URLEncoding.EncodeToString([]byte(url))
+	fmt.Println(encodedString)
+	var decodedByte, _ = base64.URLEncoding.DecodeString(encodedString)
+	var decodedString = string(decodedByte)
+	fmt.Println(decodedString)
 }
