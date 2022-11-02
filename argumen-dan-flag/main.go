84078a273844690684d0fc608c3bf64ab451768c
ilhammfadilah <ilhamfadilah92@gmail.com>
2022-11-02 16:11:05 +0700

    penggunaan flag

--- argumen-dan-flag/main.go
+++ argumen-dan-flag/main.go
@@ -1,14 +1,23 @@
 package main
 
 import (
+	"flag"
 	"fmt"
-	"os"
+	// "os"
 )
 
 func main() {
-	var argsRaw = os.Args
-	fmt.Printf("-> %#v\n", argsRaw)
+	// var argsRaw = os.Args
+	// fmt.Printf("-> %#v\n", argsRaw)
 
-	var args = argsRaw[1:]
-	fmt.Printf("-> %#v\n", args)
+	// var args = argsRaw[1:]
+	// fmt.Printf("-> %#v\n", args)
+
+	// penggunaan flag
+	var name = flag.String("name", "anonymous", "type your name")
+	var age = flag.Int64("age", 25, "type your age")
+
+	flag.Parse()
+	fmt.Printf("name \t: %s\n", *name)
+	fmt.Printf("age \t: %d", *age)
 }
