e3d25d4fb3bc4d012faa421b7b08f5fffe63e7bb
ilhammfadilah <ilhamfadilah92@gmail.com>
2022-11-03 12:11:53 +0700

    assing Reference Variabel Penampung Data

--- argumen-dan-flag/main.go
+++ argumen-dan-flag/main.go
@@ -14,10 +14,17 @@ func main() {
 	// fmt.Printf("-> %#v\n", args)
 
 	// penggunaan flag
-	var name = flag.String("name", "anonymous", "type your name")
-	var age = flag.Int64("age", 25, "type your age")
+	// var name = flag.String("name", "anonymous", "type your name")
+	// var age = flag.Int64("age", 25, "type your age")
 
-	flag.Parse()
-	fmt.Printf("name \t: %s\n", *name)
-	fmt.Printf("age \t: %d", *age)
+	// flag.Parse()
+	// fmt.Printf("name \t: %s\n", *name)
+	// fmt.Printf("age \t: %d", *age)
+
+	var data1 = flag.String("name", "anonymous", "type your name")
+	fmt.Println(*data1)
+
+	var data2 string
+	flag.StringVar(&data2, "gender", "male", "type your gender")
+	fmt.Println(data2)
 }
