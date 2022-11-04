773197baed0e2af235c9e9ee94a3405b7cdc4260
ilhammfadilah <ilhamfadilah92@gmail.com>
2022-11-04 14:21:35 +0700

    Membuat File Baru

---
+++ file/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+var path = "/home/qlhem/Latihan/dasar-pemrograman-golang/file/test.txt"
+
+func isError(err error) bool {
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	return (err != nil)
+}
+
+func createFile() {
+	var _, err = os.Stat(path)
+
+	if os.IsNotExist(err) {
+		var file, err = os.Create(path)
+		if isError(err) {
+			return
+		}
+		defer file.Close()
+	}
+
+	fmt.Println("==> file berhasil dibuat", path)
+}
+
+func main() {
+	createFile()
+}
