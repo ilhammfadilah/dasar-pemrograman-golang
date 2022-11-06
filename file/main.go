9d97908fde19b60b8c1ec616c5a1082e5c2017d3
ilhammfadilah <ilhamfadilah92@gmail.com>
2022-11-06 19:48:53 +0700

    mengedit file

--- file/main.go
+++ file/main.go
@@ -30,5 +30,33 @@ func createFile() {
 }
 
 func main() {
-	createFile()
+	// createFile()
+	writeFile()
+}
+
+func writeFile() {
+	// buka file dengan level akses READ & WRITE
+	var file, err = os.OpenFile(path, os.O_RDWR, 0644)
+	if isError(err) {
+		return
+	}
+	defer file.Close()
+
+	// tulis data ke file
+	_, err = file.WriteString("hallo\n")
+	if isError(err) {
+		return
+	}
+	_, err = file.WriteString("ayo belajar golang\n")
+	if isError(err) {
+		return
+	}
+
+	// simpan perubahan
+	err = file.Sync()
+	if isError(err) {
+		return
+	}
+
+	fmt.Println("==> file berhasil diisi")
 }
