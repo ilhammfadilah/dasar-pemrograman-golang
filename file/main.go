24f97f22c949ae7c1f9cb99dd5286760331b0a86
ilhammfadilah <ilhamfadilah92@gmail.com>
2022-11-06 19:53:09 +0700

    read file

--- file/main.go
+++ file/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"os"
 )
 
@@ -31,7 +32,8 @@ func createFile() {
 
 func main() {
 	// createFile()
-	writeFile()
+	// writeFile()
+	readFile()
 }
 
 func writeFile() {
@@ -59,4 +61,33 @@ func writeFile() {
 	}
 
 	fmt.Println("==> file berhasil diisi")
+}
+
+func readFile() {
+	// buka file
+	var file, err = os.OpenFile(path, os.O_RDONLY, 0644)
+	if isError(err) {
+		return
+	}
+	defer file.Close()
+
+	// baca file
+	var text = make([]byte, 1024)
+	for {
+		n, err := file.Read(text)
+		if err != io.EOF {
+			if isError(err) {
+				break
+			}
+		}
+		if n == 0 {
+			break
+		}
+	}
+	if isError(err) {
+		return
+	}
+
+	fmt.Println("==> file berhasil dibaca")
+	fmt.Println(string(text))
 }
