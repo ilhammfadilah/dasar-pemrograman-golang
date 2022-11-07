1df0d64e77d156d3e2cd6aadda6e4f9fd45a5d2c
ilhammfadilah <ilhamfadilah92@gmail.com>
2022-11-07 15:38:52 +0700

    penggunaan template web

--- web-server/main.go
+++ web-server/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 )
 
@@ -11,11 +12,22 @@ func index(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, "halo")
+		var data = map[string]string{
+			"Name":    "Ilham Muhamad Fadilah",
+			"Message": "Belajar golang",
+		}
+
+		var t, err = template.ParseFiles("template.html")
+		if err != nil {
+			fmt.Println(err.Error())
+			return
+		}
+
+		t.Execute(w, data)
 	})
 
 	http.HandleFunc("/index", index)
 
 	fmt.Println("starting web server at http://localhost:8000/")
-	http.ListenAndServe(":8000", nil)
+	http.ListenAndServe(":8080", nil)
 }
