75469e98ee0260ed577b0325639dfb49b9455452
ilhammfadilah <ilhamfadilah92@gmail.com>
2022-11-08 10:06:53 +0700

    Membaca 1 Record Data

--- sql/main.go
+++ sql/main.go
@@ -62,6 +62,29 @@ func sqlQuery() {
 	}
 
 }
+
+func sqlQueryRow() {
+	var db, err = connect()
+	if err != nil {
+		fmt.Println(err.Error)
+		return
+	}
+	defer db.Close()
+
+	var result = student{}
+	var id = "B001"
+	err = db.QueryRow("select name, grade from tb_student where id = ?", id).
+		Scan(&result.name, &result.grade)
+
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	fmt.Printf("name : %s\ngrade : %d\n", result.name, result.grade)
+}
+
 func main() {
-	sqlQuery()
+	// sqlQuery()
+	sqlQueryRow()
 }
