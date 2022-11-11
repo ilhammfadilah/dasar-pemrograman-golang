2dd873903fd922ba1f6ae80598fbe1a6e852e908
ilhammfadilah <ilhamfadilah92@gmail.com>
2022-11-11 09:48:40 +0700

    Comparable Data Type pada Fungsi Generic

--- go-generic/main.go
+++ go-generic/main.go
@@ -5,10 +5,16 @@ import "fmt"
 func main() {
 	total1 := Sum([]int{1, 2, 3, 4, 5})
 	fmt.Println("total1: ", total1)
+
+	total2 := Sum([]float32{2.5, 7.2})
+	fmt.Println("total2: ", total2)
+
+	total3 := Sum([]float64{1.23, 6.33, 12.6})
+	fmt.Println("total3: ", total3)
 }
 
-func Sum(numbers []int) int {
-	var total int
+func Sum[V int | float32 | float64](numbers []V) V {
+	var total V
 	for _, e := range numbers {
 		total += e
 	}
