13cc03d21e6cbc87f4b707160cbea15c6a39c365
ilhammfadilah <ilhamfadilah92@gmail.com>
2022-11-11 13:25:18 +0700

    static routing

--- b3-routing-static-asset/main.go
+++ b3-routing-static-asset/main.go
@@ -9,6 +9,6 @@ func main() {
 	http.Handle("/static/",
 		http.StripPrefix("/static/",
 			http.FileServer(http.Dir("assets"))))
-	fmt.Println("server started at localhost:9000")
-	http.ListenAndServe(":9000", nil)
+	fmt.Println("server started at localhost:7000")
+	http.ListenAndServe(":7000", nil)
 }
