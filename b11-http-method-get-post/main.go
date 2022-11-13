3fa878c1394e0eceba9a3732976bca036a0f2cd9
ilhammfadilah <ilhamfadilah92@gmail.com>
2022-11-13 07:58:40 +0700

    http method get post

---
+++ b11-http-method-get-post/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		switch r.Method {
+		case "POST":
+			w.Write([]byte("POST method"))
+		case "GET":
+			w.Write([]byte("GET method"))
+		default:
+			http.Error(w, "", http.StatusBadRequest)
+		}
+	})
+
+	fmt.Println("server started at localhost:9000")
+	http.ListenAndServe(":9000", nil)
+}
