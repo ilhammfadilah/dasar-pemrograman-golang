94dee3f41fd34c3c774746f115863a205cce6833
ilhammfadilah <ilhamfadilah92@gmail.com>
2022-11-25 14:29:09 +0700

    middleware: logrus

--- c8-advanced-middleware-logging/main.go
+++ c8-advanced-middleware-logging/main.go
@@ -3,10 +3,44 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/labstack/echo"
-	"github.com/labstack/echo/middleware"
+	log "github.com/sirupsen/logrus"
 )
+
+func MakeLogEntry(c echo.Context) *log.Entry {
+	if c == nil {
+		return log.WithFields(log.Fields{
+			"at": time.Now().Format("2006-01-02 15:04:05"),
+		})
+	}
+
+	return log.WithFields(log.Fields{
+		"at":     time.Now().Format("2006-01-02 15:04:05"),
+		"method": c.Request().Method,
+		"url":    c.Request().URL.String(),
+		"ip":     c.Request().RemoteAddr,
+	})
+}
+
+func MiddlewareLogging(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		MakeLogEntry(c).Info("incoming request")
+		return next(c)
+	}
+}
+
+func ErrorHandler(err error, c echo.Context) {
+	report, ok := err.(*echo.HTTPError)
+	if ok {
+		report.Message = fmt.Sprintf("http error %d - %v", report.Code, report.Message)
+	} else {
+		report = echo.NewHTTPError(http.StatusInternalServerError, err.Error())
+	}
+	MakeLogEntry(c).Error(report.Message)
+	c.HTML(report.Code, report.Message.(string))
+}
 
 func main() {
 	e := echo.New()
@@ -16,17 +50,37 @@ func main() {
 	e.Use(MiddlewareTwo)
 	e.Use(echo.WrapMiddleware(MiddlewareThree))
 
-	e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
-		Format: "method=${method}, uri=${uri}, status=${status}\n",
-	}))
+	e.Use(MiddlewareLogging)
+	e.HTTPErrorHandler = ErrorHandler
 
-	e.GET("/index", func(c echo.Context) (err error) {
-		fmt.Println("threeee!")
-
+	e.GET("/index", func(c echo.Context) error {
 		return c.JSON(http.StatusOK, true)
 	})
 
-	e.Logger.Fatal(e.Start(":9000"))
+	lock := make(chan error)
+	go func(locak chan error) {
+		lock <- e.Start(":9000")
+	}(lock)
+
+	time.Sleep(1 * time.Millisecond)
+	MakeLogEntry(nil).Warning("application started without ssl/lts enabled")
+
+	err := <-lock
+	if err != nil {
+		MakeLogEntry(nil).Panic("failed to start application")
+	}
+
+	// e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
+	// 	Format: "method=${method}, uri=${uri}, status=${status}\n",
+	// }))
+
+	// e.GET("/index", func(c echo.Context) (err error) {
+	// 	fmt.Println("threeee!")
+
+	// 	return c.JSON(http.StatusOK, true)
+	// })
+
+	// e.Logger.Fatal(e.Start(":9000"))
 }
 
 func MiddlewareOne(next echo.HandlerFunc) echo.HandlerFunc {
