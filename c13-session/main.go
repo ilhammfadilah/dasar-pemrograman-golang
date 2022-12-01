6eaaf2fd035e69e1ce4ea2e78c588f8495eb8543
ilhammfadilah <ilhamfadilah92@gmail.com>
2022-12-01 10:17:50 +0700

    session

---
+++ c13-session/main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/context"
+	"github.com/gorilla/sessions"
+	"github.com/labstack/echo"
+)
+
+const SESSION_ID = "id"
+
+func newCookieStrore() *sessions.CookieStore {
+	authKey := []byte("my-auth-key-very-secret")
+	encryptionKey := []byte("my-encryption-key-very-secret123")
+
+	store := sessions.NewCookieStore(authKey, encryptionKey)
+
+	store.Options.Path = "/"
+	store.Options.MaxAge = 86400 * 7
+	store.Options.HttpOnly = true
+
+	return store
+}
+
+func main() {
+	store := newCookieStrore()
+	e := echo.New()
+	e.Use(echo.WrapMiddleware(context.ClearHandler))
+
+	e.GET("/set", func(c echo.Context) error {
+		session, _ := store.Get(c.Request(), SESSION_ID)
+		session.Values["message1"] = "Hello"
+		session.Values["message2"] = "World"
+		session.Save(c.Request(), c.Response())
+
+		return c.Redirect(http.StatusTemporaryRedirect, "/get")
+	})
+
+	e.GET("/get", func(c echo.Context) error {
+		session, _ := store.Get(c.Request(), SESSION_ID)
+
+		if len(session.Values) == 0 {
+			return c.String(http.StatusOK, "empty result")
+		}
+
+		return c.String(http.StatusOK, fmt.Sprintf(
+			"%s %s",
+			session.Values["message1"],
+			session.Values["message2"],
+		))
+	})
+
+	e.GET("/delete", func(c echo.Context) error {
+		session, _ := store.Get(c.Request(), SESSION_ID)
+		session.Options.MaxAge = -1
+		session.Save(c.Request(), c.Response())
+		return c.Redirect(http.StatusTemporaryRedirect, "/get")
+	})
+	e.Logger.Fatal(e.Start(":9000"))
+}
