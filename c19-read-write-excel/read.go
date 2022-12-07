4d9ed03238772c3b9f21504fcc23f8f65eed631c
ilhammfadilah <ilhamfadilah92@gmail.com>
2022-12-07 20:11:29 +0700

    read

---
+++ c19-read-write-excel/read.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/360EntSecGroup-Skylar/excelize"
+)
+
+type M map[string]interface{}
+
+func main() {
+	xlsx, err := excelize.OpenFile("./file1.xlsx")
+	if err != nil {
+		log.Fatal("Error", err.Error())
+	}
+
+	sheet1Name := "Sheet One"
+
+	rows := make([]M, 0)
+	for i := 2; i < 5; i++ {
+		row := M{
+			"Name":   xlsx.GetCellValue(sheet1Name, fmt.Sprintf("A%d", i)),
+			"Gender": xlsx.GetCellValue(sheet1Name, fmt.Sprintf("B%d", i)),
+			"Age":    xlsx.GetCellValue(sheet1Name, fmt.Sprintf("C%d", i)),
+		}
+		rows = append(rows, row)
+	}
+
+	fmt.Printf("%v \n", rows)
+}
